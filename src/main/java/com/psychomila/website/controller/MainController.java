2c331263959f3842e052848b52c943f1af2ac5b6
n4mej <next.too642@gmail.com>
2026-02-22 22:00:13 +0300

    first commit

---
+++ src/main/java/com/psychomila/website/controller/MainController.java
@@ -0,0 +1,30 @@
+package com.psychomila.website.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Главная");
+        model.addAttribute("activePage", "home");
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "О себе");
+        model.addAttribute("activePage", "about");
+        return "about";
+    }
+
+    @GetMapping("/contacts")
+    public String contacts(Model model) {
+        model.addAttribute("title", "Контакты");
+        model.addAttribute("activePage", "contacts");
+        return "contacts";
+    }
+}
